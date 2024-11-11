bda88183dbe1d55b29fd95b8ca54e381c3f54983
Roman Bodnar <Roman.Bodnar.ShI.2023@lpnu.ua>
2024-11-11 19:03:02 +0200

    add html

--- Library/urls.py
+++ Library/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from library_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('canva/', views.canva_view, name='canva_view'),
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def canva_view(request):
+    return render(request, 'canva_template.html')
