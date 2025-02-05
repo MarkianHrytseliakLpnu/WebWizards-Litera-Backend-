27050a8e7bef093661fcf3623aa2c2b3d745028b
erik.parrot <erik.savchyn.shi.2023@lpnu.ua>
2025-02-05 21:57:12 +0200

    was updated serializers, urls and views

--- library_app/serializers.py
+++ library_app/serializers.py
@@ -1,27 +1,32 @@
 from rest_framework import serializers
 from .models import User, Location, Book, TradeLog, Review
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'surname', 'email', 'phone_number']
+        fields = '__all__'
+
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = ['id', 'name', 'address', 'work_schedule']
+        fields = '__all__'
+
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'location', 'name', 'author', 'publishing', 'year_of_publication', 'language', 'number_of_pages']
+        fields = '__all__'
+
 
 class TradeLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = TradeLog
-        fields = ['id', 'user', 'brought_book', 'borrowed_book', 'location', 'trade_time']
+        fields = '__all__'
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'author', 'book', 'rating', 'response', 'writing_time']
+        fields = '__all__'
--- library_app/views.py
+++ library_app/views.py
@@ -1,91 +1,42 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponse
+from django.views import View
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import User, Location, Book, TradeLog, Review
-from .serializers import UserSerializer, LocationSerializer, BookSerializer, TradeLogSerializer, ReviewSerializer
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from library_app.models import User, Book
-from django.views import View
 
-class UserListCreateView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+from .models import Book, TradeLog, Review
+from .serializers import UserSerializer, BookSerializer, TradeLogSerializer, ReviewSerializer
 
+
+class HomeView(View):
     def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+        return render(request, 'home.html')
+
+
+class BooksView(View):
+    def get(self, request):
+        books = Book.objects.all()
+        return render(request, 'book.html', {'books': books})
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        return render(request, 'user_register.html')
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = UserSerializer(data=request.POST)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class UserDetailView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+            return redirect('home')
+        else:
+            return HttpResponse(f"Помилка: {serializer.errors}", status=status.HTTP_400_BAD_REQUEST)
 
 
-# Views for Location
-class LocationListCreateView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+# ---------- Books Endpoints ----------
 
-    def get(self, request):
-        locations = Location.objects.all()
-        serializer = LocationSerializer(locations, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = LocationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class LocationDetailView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get(self, request, pk):
-        location = get_object_or_404(Location, pk=pk)
-        serializer = LocationSerializer(location)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        location = get_object_or_404(Location, pk=pk)
-        serializer = LocationSerializer(location, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        location = get_object_or_404(Location, pk=pk)
-        location.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# Views for Book
 class BookListCreateView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
@@ -100,6 +51,7 @@ class BookListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class BookDetailView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -123,45 +75,8 @@ class BookDetailView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# Views for TradeLog
-class TradeLogListCreateView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+# ---------- Reviews Endpoints ----------
 
-    def get(self, request):
-        tradelogs = TradeLog.objects.all()
-        serializer = TradeLogSerializer(tradelogs, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = TradeLogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class TradeLogDetailView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get(self, request, pk):
-        tradelog = get_object_or_404(TradeLog, pk=pk)
-        serializer = TradeLogSerializer(tradelog)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        tradelog = get_object_or_404(TradeLog, pk=pk)
-        serializer = TradeLogSerializer(tradelog, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        tradelog = get_object_or_404(TradeLog, pk=pk)
-        tradelog.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# Views for Review
 class ReviewListCreateView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
@@ -176,6 +91,7 @@ class ReviewListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ReviewDetailView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -199,23 +115,41 @@ class ReviewDetailView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class HomeView(View):
-    def get(self, request):
-        return render(request, 'home.html')
+# ---------- Rent (TradeLog) Endpoints ----------
 
-class BooksView(View):
-    def get(self, request):
-        books = Book.objects.all()
-        return render(request, 'book.html', {'books': books})
+class TradeLogListCreateView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-class UserRegisterView(View):
     def get(self, request):
-        return render(request, 'user_register.html')
+        tradelogs = TradeLog.objects.all()
+        serializer = TradeLogSerializer(tradelogs, many=True)
+        return Response(serializer.data)
 
     def post(self, request):
-        serializer = UserSerializer(data=request.POST)
+        serializer = TradeLogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return redirect('home')
-        else:
-            return HttpResponse(f"Помилка: {serializer.errors}", status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TradeLogDetailView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, pk):
+        tradelog = get_object_or_404(TradeLog, pk=pk)
+        serializer = TradeLogSerializer(tradelog)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        tradelog = get_object_or_404(TradeLog, pk=pk)
+        serializer = TradeLogSerializer(tradelog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        tradelog = get_object_or_404(TradeLog, pk=pk)
+        tradelog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
